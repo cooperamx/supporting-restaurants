a9e3079a5d6abc951565fc627e6e31a022f01c80
Alberto Romero <lguitarras0594@gmail.com>
2020-03-29 21:02:05 -0600

    Add restaurant item without style and grid

---
+++ components/RestaurantItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="product-item card column is-one-quarter" @click="$emit('click')">
+    <img
+      class="product-item__photo"
+      :src="image"
+    />
+    <p class="product-item__name">
+      {{ name }}
+    </p>
+    </p class="product-item__location">
+      {{ location }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductItem',
+
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.product-item {
+  cursor: pointer;
+  padding: 16px;
+  color: #7E7E7E;
+}
+
+.product-item__photo {
+  width: 230px;
+  height: 208px;
+  border-radius: 16px;
+}
+
+.product-item__name {
+  font-weight: bold;
+}
+</style>
