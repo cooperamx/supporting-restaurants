c8f1dee0ec6ef6fcbc55ddd44bf3126e9700d0f0
Iddar Olivares <iddar@dbug.mx>
2020-03-29 21:12:14 -0600

    feat: include header basic design

--- components/Header.vue
+++ components/Header.vue
@@ -1,21 +1,51 @@
 <template>
-  <section class="hero is-primary">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">
-          Hero title
+  <section class="hero">
+    <div class="hero-body is-vcentered">
+      <div class="container has-text-centered">
+        <h1 class="title __title">
+          Coopera.mx
         </h1>
-        <h2 class="subtitle">
-          Hero subtitle
-        </h2>
+        <div class="columns">
+          <div class="column is-half is-offset-one-quarter">
+            <h2 class="subtitle">
+              Queremos ayudar a nuestros restaurantes locales a tener un medio para hacer preventas o recibir donaciones de manera digital, y así apoyarlos en la crisis.
+            </h2>
+            <div>
+              <Buttom text="Soy un restaurante" primary />
+              <Buttom text="Ver restaurantes" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
+<script>
+import Buttom from './ui/Buttom'
+
+export default {
+  components: {
+    Buttom
+  }
+}
+</script>
+
 <style scoped>
-.__header {
+.hero {
   background-color: var(--head-bg);
+  color: var(--main-text);
+  height: 700px;
+}
+
+.hero-body.is-vcentered {
+  align-items: center;
+  justify-content: center;
+  display: flex;
+}
+
+.hero .title {
   color: var(--primary);
+  font-size: 3rem;
 }
 </style>
---
+++ components/ui/Buttom.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    v-bind:class="{
+      __buttom : true,
+      __primary: primary
+    }"
+  >
+    {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+    primary: Boolean
+  }
+}
+</script>
+
+<style scoped>
+.__buttom {
+  background-color: var(--main-bg);
+  border-radius: 2rem;
+  color: var(--primary);
+  display: inline-block;
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin: .5rem;
+  padding: .5rem 1rem;
+}
+
+.__primary {
+  background-color: var(--primary);
+  color: var(--main-bg);
+}
+</style>
