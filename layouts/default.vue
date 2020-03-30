986140b96635101b7c0deb58f3ab7eaed11c7bd4
Alan <alanmunguiacerda@hotmail.com>
2020-03-29 20:43:15 -0600

    Fix default font-family

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,4 @@
 </template>
 
 <style>
-html {
-  font-family: 'Poppins', sans-serif, 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-}
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,7 @@ export default {
   ** Global CSS
   */
   css: [
+    { src: '~/assets/main.scss', lang: 'scss' }
   ],
   /*
   ** Plugins to load before mounting the App
