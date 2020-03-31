53ccd6b0636df3a48103734181a20b1989ee6050
Alan <alanmunguiacerda@hotmail.com>
2020-03-30 21:55:47 -0600

    Add Link component

---
+++ components/ui/Link.vue
@@ -0,0 +1,22 @@
+<template>
+  <a class="link is-size-7" :href="href">
+    <span>{{ label }}</span>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    href: String,
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.link
+  color: var(--primary)
+  &:hover
+    color: var(--primary)
+    text-decoration: underline
+</style>
