323535576ff3b43b069cfb2db8329d4e1b62180c
Alan <alanmunguiacerda@hotmail.com>
2020-03-30 22:49:23 -0600

    Add CompanyHero component

--- components/CompanyHero.vue
+++ components/CompanyHero.vue
@@ -1,47 +1,73 @@
 <template>
-  <header class="header">
-    <div class=""></div>
-  </header>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container is-widescreen">
+        <Header />
+        <div class="section">
+          <Link label="< Volver a inicio" href="/" />
+        </div>
+        <div class="section company-info columns is-mobile">
+          <div class="column is-narrow">
+            <figure class="image is-96x96">
+              <img :src="logo">
+            </figure>
+          </div>
+          <div class="column is-text-left">
+            <h1 class="is-size-3 has-text-weight-bold">{{ name }}</h1>
+            <p class="is-size-7">{{ address }}</p>
+            <Link label="Visitar sitio web" :href="website" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import Header from './ui/Header';
+import Link from './ui/Link';
 import Buttom from './ui/Buttom';
 
 export default {
   components: {
+    Header,
+    Link,
     Buttom
-  }
+  },
+  props: {
+    name: String,
+    address: String,
+    website: String,
+    logo: String,
+  },
 };
 </script>
 
-<style scoped>
-.header {
-  background-color: var(--head-bg);
-  color: var(--main-text);
-  height: 272px;
-  padding: 16px 0 0 32px;
-}
+<style lang="sass" scoped>
+@import '~bulma/sass/utilities/_all'
 
-.hero .title {
-  color: var(--primary);
-  font-size: 3rem;
-  font-weight: 800;
-  font-family: 'Poppins';
-}
+.hero
+  background-color: var(--head-bg)
+  color: var(--main-text)
+  height: 17rem
 
-.__glich {
-  align-items: flex-start;
-  display: flex;
-  height: 80px;
-  justify-content: center;
-  overflow: hidden;
-  margin-bottom: -10px;
-}
+.hero-body
+  padding: 1rem 1rem 2rem 1rem
 
-.__circle {
-  background: var(--main-bg);
-  border-radius: 200% 200% 0 0;
-  height: 130px;
-  width: 100vw;
-}
+.section
+  padding: 0 0 1.5rem 0
+
+.company-info
+  color: #3E3E3E
+
+.image
+  border: solid 0.25rem white
+  border-radius: 0.25rem
+
+.link
+  color: var(--primary)
+
++desktop()
+  .section
+    padding: 0 1.5rem 1.5rem 1.5rem
 </style>
