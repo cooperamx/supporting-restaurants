c79ee423f878ad0e5b17856fc8f3c2e86bcb3c38
Iddar Olivares <iddar@dbug.mx>
2020-04-04 15:54:54 -0600

    feat: update restaurants db

--- libs/dbStatic.js
+++ libs/dbStatic.js
@@ -1,58 +1,115 @@
 export const restaurants = [
   {
-    uri: "louieburger",
-    name: "Louie Burger",
-    image:
-      "/business/boroder_001.jpg",
+    uri: "boroder",
+    name: "Boroder ",
+    image: "/business/boroder_001.jpg",
     location: "Guadalajara, Jalisco",
-    website: "https://louie-burguer.com.mx",
-    phone: "526421150094",
+    website: "https://Boroder beer",
+    bio: "Barecito casual",
+    phone: "",
     coupons: [{
-      title: 'A1 Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
+      title: 'Tarjetas de regalo de 100',
+      body: 'Certificado de regalo por $100.',
     }, {
-      title: 'B3 Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
+      title: 'Tarjetas de regalo de 200',
+      body: 'Certificado de regalo por $200.',
     }, {
-      title: 'C5 Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
-    }, {
-      title: 'Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
-    }, {
-      title: 'Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
-    }, {
-      title: 'Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
+      title: 'Tarjetas de regalo de 500',
+      body: 'Certificado de regalo por $500.',
     }]
   },
   {
-    uri: "cafecaligari",
-    name: "Cafe caligari",
-    image:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5ByNypP6wStW4FH7shSbitq8YW8QXGmHotyZag21v43GymbuT&usqp=CAU",
+    uri: "cafe_correcto",
+    name: "Café Correcto",
+    image: "/business/cafe_correcto_002.jpg",
     location: "Guadalajara, Jalisco",
-    website: "https://www.facebook.com/caligaricafe/",
-    phone: "5213311804588",
+    website: "http://correcto.coffee",
+    bio: "Somos una barra de café de especialidad en la colonia Americana, nuestra premisa es muy sencilla, servir un café bien producido, bien comprado y bien preparado en cada taza. Tenemos también alimentos, postres, cerveza artesanal y vermut.",
+    phone: "",
     coupons: [{
-      title: 'Z6 Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
+      title: 'Tarjetas de regalo de 100',
+      body: 'Certificado de regalo por $100.',
     }, {
-      title: 'R4 Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
+      title: 'Tarjetas de regalo de 200',
+      body: 'Certificado de regalo por $200.',
     }, {
-      title: 'P7 Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
+      title: 'Tarjetas de regalo de 500',
+      body: 'Certificado de regalo por $500.',
+    }]
+  },
+  {
+    uri: "mexico_dulce_y_que_rico",
+    name: "México Dulce y Que Rico",
+    image: "/business/mexico_dulce_que_rico_003.jpg",
+    location: "Estado de mexico",
+    website: "https://www.instagram.com/mexicodulceoficial/",
+    bio: "Creamos experiencias basadas en dulces tradicionales mexicanos, con el fin de que los compartas con tus amigos y familiares. Siempre apoyando a pequeños artesanos y productores mexicanos, de dulces y artesanías.",
+    phone: "",
+    coupons: [{
+      title: 'Tarjetas de regalo de 100',
+      body: 'Certificado de regalo por $100. valido apartir del Lunes 6 de Abril de 2020 y hasta el 31 de Diciembre de 2020',
     }, {
-      title: 'Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
+      title: 'Tarjetas de regalo de 200',
+      body: 'Certificado de regalo por $200. valido apartir del Lunes 6 de Abril de 2020 y hasta el 31 de Diciembre de 2020',
     }, {
-      title: 'Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
+      title: 'Tarjetas de regalo de 500',
+      body: 'Certificado de regalo por $500. valido apartir del Lunes 6 de Abril de 2020 y hasta el 31 de Diciembre de 2020',
+    }]
+  }, {
+    uri: "cafe_oro_maya",
+    name: "Café Oro Maya",
+    image: "/business/oro_maya_004.png",
+    location: "Guadalajara, Jalisco",
+    website: "http://eltallercerveceria.mx/",
+    bio: "Café de Especialidad Orgánico Certificado de la Finca Irlanda, Chiapas: Expendio y Cafetería.",
+    phone: "",
+    coupons: [{
+      title: 'Descuento 10%',
+      body: '10% de Descuento en Bebidas al mencionar que supieron de nosotros en COOPERA.MX.',
+    }]
+  },
+  {
+    uri: "el_taller_cervecería",
+    name: "El Taller Cervecería",
+    image: "/business/taller_cerveceria_005.png",
+    location: "Azcapotzalco",
+    website: "http://eltallercerveceria.mx/",
+    bio: "En el Taller Cervecería encontrarás un tributo a todos aquellos “maestros” mecánicos de nuestro México. Su concepto, un homenaje a su historia y un regalo a nuestra gastronomía mexicana, que más allá de un taco sudado, es la vida de cientos de mexicanos día a día. ",
+    phone: "",
+    coupons: [{
+      title: 'Tarjetas de regalo de 100',
+      body: 'Certificado de regalo por $100.',
     }, {
-      title: 'Llévate 2 por el precio de una y media',
-      body: 'In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telesc.',
+      title: 'Tarjetas de regalo de 200',
+      body: 'Certificado de regalo por $200.',
+    }, {
+      title: 'Tarjetas de regalo de 500',
+      body: 'Certificado de regalo por $500.',
+    }, {
+      title: 'Tarjetas de regalo de 1000',
+      body: 'Certificado de regalo por $1000.',
+    }]
+  },
+  {
+    uri: "healthy_life",
+    name: "Healthy Life",
+    image: "/business/healthy_life_006.jpg",
+    location: "Ciudad Obregón, Sonora",
+    website: "http://eltallercerveceria.mx/",
+    bio: "Nos dedicamos al cuidado integral de tu salud, ofreciendo alimentos orgánicos, productos naturales, accesorios eco friendly y el servicio de análisis de resonancia magnética.",
+    phone: "",
+    coupons: [{
+      title: '10% adicional',
+      body: 'Compra $300 y recibe el 10% adicional.',
+    }, {
+      title: '15% de crédito adicional',
+      body: 'Adquiere tu Healthy Gift Card y recibe 15% de crédito adicional',
+    }, {
+      title: 'Popote o bolsa de regalo',
+      body: 'Compra $500 y recibe popote o bolsa reutilizable a escoger.',
+    }, {
+      title: 'Pasta dental ecológica de regalo',
+      body: 'En tu compra mayor a $1000 recibe gratis pasta dental ecológica.',
     }]
   }
 ]
