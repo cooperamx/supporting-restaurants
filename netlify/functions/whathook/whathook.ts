2a063109cf41aa816710f7c5d36c2fb738d8639e
Iddar Olivares <iddar@dbug.mx>
2023-02-21 00:03:04 -0600

    feat: log

--- netlify/functions/whathook/whathook.ts
+++ netlify/functions/whathook/whathook.ts
@@ -3,18 +3,25 @@ import { Handler } from '@netlify/functions'
 const token = process.env.WS_TOKEN
 
 export const handler: Handler = async (event, context) => {
-  if (
-    event.queryStringParameters &&
-    event.queryStringParameters['hub.mode'] == 'subscribe' &&
-    event.queryStringParameters['hub.verify_token'] == token
-  ) {
-    return {
-      statusCode: 200,
-      body: event.queryStringParameters['hub.challenge'],
-    }
-  } else {
-    return {
-      statusCode: 400
-    }
+  // parse the request body into a useable format (JSON)
+  if(event.body) {
+    const body = JSON.parse(event.body)
+    console.log({body})
   }
+
+  // if (
+  //   event.queryStringParameters &&
+  //   event.queryStringParameters['hub.mode'] == 'subscribe' &&
+  //   event.queryStringParameters['hub.verify_token'] == token
+  // ) {
+  //   return {
+  //     statusCode: 200,
+  //     body: event.queryStringParameters['hub.challenge'],
+  //   }
+  // } else {
+  //   return {
+  //     statusCode: 400
+  //   }
+  // }
+  
 }
