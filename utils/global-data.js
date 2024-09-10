6f23f46a59f286211f5b4ae2226762d45c0502c6
Iddar Olivares <iddar@dbug.mx>
2024-09-10 16:43:05 -0600

    feat: update titel

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'coopera.mx';
+    : 'coopera.mx - Blog';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Entre café, networking & mujeres';
